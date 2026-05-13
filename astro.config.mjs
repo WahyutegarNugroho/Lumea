840054b21a4763324b5d945e8d66d0ca4dd0e535
WahyutegarNugroho <wahyutgrss@gmail.com>
2026-05-13 14:17:13 +0700

    chore: setup Google Search Console verification and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lumea-whtsn.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   i18n: {
     defaultLocale: 'en',
